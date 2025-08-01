3654139979e1f61a05c927b1e4526253bb29761e
rajiss-ctrl <omosanjos77@gmail.com>
2025-07-31 18:57:44 -0700

    block facebook login

--- app/components/SocialAuthButtons.tsx
+++ app/components/SocialAuthButtons.tsx
@@ -1,19 +1,19 @@
-'use client';
+// 'use client';
 
-import { FaFacebook } from "react-icons/fa";
-import { initiateFacebookLogin } from "../lib/appwrite";
-
-
+// import { FaFacebook } from "react-icons/fa";
+// import { initiateFacebookLogin } from "../lib/appwrite";
 
 
-export default function SocialAuthButtons() {
-  return (
-    <button 
-      onClick={initiateFacebookLogin}
-      className="bg-blue-600 text-white px-4 py-2 rounded flex items-center gap-2"
-    >
-      <FaFacebook/>
-      Continue with Facebook
-    </button>
-  );
-}
+
+
+// export default function SocialAuthButtons() {
+//   return (
+//     <button 
+//       onClick={initiateFacebookLogin}
+//       className="bg-blue-600 text-white px-4 py-2 rounded flex items-center gap-2"
+//     >
+//       <FaFacebook/>
+//       Continue with Facebook
+//     </button>
+//   );
+// }
