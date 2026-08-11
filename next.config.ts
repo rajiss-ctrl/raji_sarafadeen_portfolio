11965a98084ab7541632ca47ff96361120e0f88e
rajiss-ctrl <omosanjos77@gmail.com>
2026-08-10 17:40:14 -0700

    Resolve: deprcated images.domain

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      'res.cloudinary.com', // Cloudinary domain
-      // Add other domains if needed
-    ],
     // Optional: For more advanced configurations
     remotePatterns: [
       {
